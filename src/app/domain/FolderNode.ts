2cdd27e654107eb0ca2714f7b419a7527a2a52e9
Michael H. Pedersen <connect@dotnector.dk>
2026-07-01 09:41:37 +0200

    adding a few summaries

--- src/app/domain/FolderNode.ts
+++ src/app/domain/FolderNode.ts
@@ -3,6 +3,15 @@
 /// </summary>
 import { FolderObjectRef } from "./FolderObjectRef";
 
+/**
+ * Represents a folder node in a hierarchical structure, containing references to child folders and associated objects.
+ * @param id - The unique identifier for the folder node.
+ * @param name - The display name of the folder node.
+ * @param icon - The icon associated with the folder node.
+ * @param parentId - Reference to parent folder, if any. Root folders will have a null parentId.
+ * @param children - An array of unique identifiers for child folder nodes.
+ * @param objects - An array of references to Home Assistant entities contained within the folder node.
+ */
 export class FolderNode {
   public readonly id: string;
   public name: string;
